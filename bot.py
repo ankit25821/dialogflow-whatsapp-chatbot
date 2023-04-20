5a8b12a16a397ab9713818c04760286e0e7382ca
Ankit Tiwari <67471937+ankit25821@users.noreply.github.com>
2023-04-20 16:13:33 +0530

    Improved code formating

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from flask import Flask, request
 import os
 import dialogflow
@@ -5,7 +7,7 @@ from google.api_core.exceptions import InvalidArgument
 from twilio.twiml.messaging_response import MessagingResponse
 import requests
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] =  'private_key.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'private_key.json'
 
 DIALOGFLOW_PROJECT_ID = 'project_id'
 DIALOGFLOW_LANGUAGE_CODE = 'en'
@@ -17,41 +19,49 @@ app.config['DEBUG'] = True
 
 @app.route('/')
 def root():
-    return "WhatsApp Chatbot"
+    return 'WhatsApp Chatbot'
+
 
 @app.route('/api/getMessage', methods=['POST'])
 def home():
-    message=request.form.get('Body')
-    mobnum=request.form.get('From')
+    message = request.form.get('Body')
+    mobnum = request.form.get('From')
     session_client = dialogflow.SessionsClient()
-    session = session_client. session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
-    text_input = dialogflow.types. TextInput(text=message, language_code=DIALOGFLOW_LANGUAGE_CODE)
-    query_input = dialogflow. types. QueryInput(text=text_input)
+    session = session_client.session_path(DIALOGFLOW_PROJECT_ID,
+            SESSION_ID)
+    text_input = dialogflow.types.TextInput(text=message,
+            language_code=DIALOGFLOW_LANGUAGE_CODE)
+    query_input = dialogflow.types.QueryInput(text=text_input)
     try:
-        response = session_client.detect_intent(session=session, query_input=query_input)
+        response = session_client.detect_intent(session=session,
+                query_input=query_input)
     except InvalidArgument:
         raise
-    print("Query text:", response.query_result.query_text)
-    print("Detected intent:", response.query_result.intent.display_name)
-    print("Detected intent confidence:", response.query_result. intent_detection_confidence)
-    print("Fulfillment text:", response.query_result. fulfillment_text)
+    print ('Query text:', response.query_result.query_text)
+    print ('Detected intent:',
+           response.query_result.intent.display_name)
+    print ('Detected intent confidence:',
+           response.query_result.intent_detection_confidence)
+    print ('Fulfillment text:', response.query_result.fulfillment_text)
+
     # sendMessage(mobnum, response.query_result. fulfillment_text)
-    return response.query_result. fulfillment_text
+
+    return response.query_result.fulfillment_text
 
 
 def sendMessage(mobnum, message):
-    print("Mobile number : ",mobnum)
-    print("Message is : ",message)
-    url = "https://api.twilio.com/2010-04-01/Accounts/AC3b4e9356a0acfb55cceeece7f5752f19/Messages.json"
-    payload = {'From': 'whatsapp:+14123456789',
-    'Body': message,
-    'To': mobnum}
-    headers = {
-        'Authorization': 'Basic <token>'
-    }
-    response = requests.request('POST', url, headers=headers, data=payload)
-    print(response.text.encode('utf8'))
-    return ""
+    print ('Mobile number : ', mobnum)
+    print ('Message is : ', message)
+    url = \
+        'https://api.twilio.com/2010-04-01/Accounts/AC3b4e9356a0acfb55cceeece7f5752f19/Messages.json'
+    payload = {'From': 'whatsapp:+14123456789', 'Body': message,
+               'To': mobnum}
+    headers = {'Authorization': 'Basic <token>'}
+    response = requests.request('POST', url, headers=headers,
+                                data=payload)
+    print response.text.encode('utf8')
+    return ''
+
 
 if __name__ == '__main__':
     app.run()
